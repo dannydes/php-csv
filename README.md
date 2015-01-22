d306037bda3f84c92d71804619e796b24b89225c
Daniel <desiradaniel2007@hotmail.com>
2015-01-22 15:59:56 +0100

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+php-csv
+-------
+
+## Examples
+
+$csv = CSV::getInstance();
+var_dump($csv->decode("Betapsi - Psychology Students' Association,President: Paula Caruana,betapsimalta@gmail.com,http://betapsi.org.mt/,https://www.facebook.com/BetaPsiMalta;
+AEGEE-Valletta - The European Students' Forum,President: Christian Frendo,info@aegee-valletta.org,http://www.aegee-valletta.org,http://www.facebook.com/AEGEE.Valletta;
+"));
---
+++ php-csv.php
@@ -0,0 +1,38 @@
+<?php
+
+interface iCSV {
+	public function decode($string);
+	public function encode($array);
+
+	public function decode_row($string);
+}
+
+class CSV implements iCSV {
+	private static $instance = NULL;
+
+	private function __construct() { }
+
+	public function getInstance() {
+		if (self::$instance === NULL) {
+			self::$instance = new CSV();
+		}
+		return self::$instance;
+	}
+
+	public function decode_row($string) {
+		return explode(',', $string);
+	}
+
+	public function decode($string) {
+		//separate into rows
+		$rows = explode(';', $string);
+		$rows = array_map('trim', $rows);
+
+		//separate rows into columns
+		return array_map(array($this, 'decode_row'), $rows);
+	}
+
+	public function encode($array) {
+
+	}
+}
